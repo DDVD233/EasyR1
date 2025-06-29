56de19e1d95a7b8e55c3c6506f116e6f4efc4228
DVD <zjdavid.2003@gmail.com>
2025-06-29 16:42:24 -0400

    debug

--- verl/utils/dataset.py
+++ verl/utils/dataset.py
@@ -518,6 +518,8 @@ class RLHFDataset(Dataset):
             vision_path = example['time_series'][0] if 'time_series' in example and len(example['time_series']) != 0 else None
             is_timeseries = True
         prompt_str = example[self.prompt_key]
+        if 'demo' in example:
+            print(f"Demo example found: {example['demo']}. Using prompt: {prompt_str}")
 
         if 'How long will the patient stay in the hospital?' in prompt_str:
             example["data_source"] = "multimodal"
