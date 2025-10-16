8390008d71e1e67a023d1544deb1054a43b6d5d8
DVD <zjdavid.2003@gmail.com>
2025-10-16 12:07:21 -0400

    Update streamlit api

--- streamlit_demo/app.py
+++ streamlit_demo/app.py
@@ -314,7 +314,7 @@ with left_col:
                                 image_data = image_url.split(",")[1]
                                 image_bytes = base64.b64decode(image_data)
                                 image = Image.open(BytesIO(image_bytes))
-                                st.image(image, use_container_width=True)
+                                st.image(image, width='stretch')
                     elif isinstance(item, str):
                         st.markdown(item)
             else:
